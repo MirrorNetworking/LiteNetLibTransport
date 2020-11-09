7501be14a42f6d372b8c1e51092c15dd89b2a4c1
James Frowen <jamesfrowendev@gmail.com>
2020-11-09 19:39:52 +0000

    feat: adding PreprocessorDefines

---
+++ source/LiteNetLibTransportPreprocessorDefine.cs
@@ -0,0 +1,32 @@
+// need this defile so that this script can be in the runtime asmdef (avoids needing to make 2nd asmdef just for this 1 script)
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace LiteNetLibMirror
+{
+    static class LiteNetLibTransportPreprocessorDefine
+    {
+        /// <summary>
+        /// Add define symbols as soon as Unity gets done compiling.
+        /// </summary>
+        [InitializeOnLoadMethod]
+        public static void AddDefineSymbols()
+        {
+            string currentDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+            HashSet<string> defines = new HashSet<string>(currentDefines.Split(';'))
+            {
+                "LITENETLIB_TRANSPORT"
+            };
+
+            // only touch PlayerSettings if we actually modified it.
+            // otherwise it shows up as changed in git each time.
+            string newDefines = string.Join(";", defines);
+            if (newDefines != currentDefines)
+            {
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, newDefines);
+            }
+        }
+    }
+}
+#endif
